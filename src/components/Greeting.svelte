a6832057dcf57ddc894e75c5a8d2ffc3349c22e8
Erik Verduin <everduin@cisco.com>
2023-01-01 15:34:59 -0600

    Finished tutorial. Clean up styles. Clean up filler code.

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,11 @@
+<script>
+
+  let value = 0;
+
+  function handleClick() {
+     value += 1
+  }
+
+</script>
+
+<button on:click={handleClick} class="bg-green-500 text-white px-2 py-1 rounded-sm active:scale-[.98]">Astro Island: {value}</button>
