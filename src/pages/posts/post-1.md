efa4ffb10231a2e42c0f2faeaf69c5493a239eaa
Erik Verduin <everduin@cisco.com>
2023-01-02 12:02:02 -0600

    Need to replace filler blog post with roadmap, otherwise blog looks good

--- src/pages/posts/post-1.md
+++ src/pages/posts/post-1.md
@@ -21,3 +21,12 @@ tags: ["astro", "blogging", "learning in public", "wrap me"]
 ## What's next
 
 I will finish the Astro tutorial, and then keep adding more posts. Watch this space for more to come.
+
+
+## What I've accomplished
+
+1. **Installing Astro**: First, I created a new Astro project and set up my online accounts.
+
+2. **Making Pages**: I then learned how to make pages by creating new `.astro` files and placing them in the `src/pages/` folder.
+
+3. **Making Blog Posts**: This is my first blog post! I now have Astro pages and Markdown posts!
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,23 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-typography: {
-        DEFAULT: {
-          css: {
-          }}}
-    },
+     gridTemplateColumns: {
+        "left": "max-content minmax(0, 1fr)",
+        "right": "minmax(0, 1fr) max-content",
+        "mid": "minmax(0, 1fr) max-content minmax(0, 1fr)",
+     },
+     gridTemplateRows: {
+        "bottom": "max-content minmax(0, 1fr)",
+        "top": "minmax(0, 1fr) max-content",
+        "top-2": "1fr max-content",
+        "mid": "minmax(0, 1fr) max-content minmax(0, 1fr)",
+     },
+     typography: {
+         DEFAULT: {
+           css: {}
+       }
+     }
+   },
 	},
-	plugins: [require('@tailwindcss/typography'), require('@catppuccin/tailwindcss')],
+	plugins: [require('@tailwindcss/typography')],
 }
