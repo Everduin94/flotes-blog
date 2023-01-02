439cd2a9cbb7f4ff82f65a2b9dcc0d18b517494d
Erik Verduin <everduin@cisco.com>
2023-01-02 09:03:43 -0600

    post layout fix, theme color fix. mid-work need to add discord and webapp link as footer or header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+typography: {
+        DEFAULT: {
+          css: {
+          }}}
+    },
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography'), require('@catppuccin/tailwindcss')],
 }
